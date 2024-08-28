4230fc0fc5460db185d73532c47690cb8ce07e30
Lydia <lydiazly2@gmail.com>
2024-08-28 12:32:49 -0700

    updated hooks

--- src/hooks/useDebouncedFetchNameSuggestions.js
+++ src/hooks/useDebouncedFetchNameSuggestions.js
@@ -1,6 +1,6 @@
 // src/hooks/useDebouncedFetchSuggestions.js
 import { useMemo } from 'react';
-import fetchSuggestions from '../utils/fetchSuggestions';
+import fetchNameSuggestions from '../utils/fetchNameSuggestions';
 import debounce from 'lodash/debounce';
 
 const useDebouncedFetchSuggestions = (
@@ -17,19 +17,16 @@ const useDebouncedFetchSuggestions = (
         async (query, serviceChosen) => {
           if (query && !isSelecting.current) {
             try {
-              dispatch({ type: actionTypes.SET_SUGGESTIONS_LOADING_ON });
               const requestId = ++latestSuggestionRequest.current;  // Increment and capture the current request ID
 
-              const suggestions = await fetchSuggestions(query, serviceChosen);
+              const suggestions = await fetchNameSuggestions(query);
 
               /* Only update if this request is the latest one */
               if (requestId === latestSuggestionRequest.current) {
                 dispatch({ type: actionTypes.SET_SUGGESTIONS, payload: suggestions });
               }
             } catch (error) {
-              setErrorMessage((prev) => ({ ...prev, location: error.message }));
-            } finally {
-              dispatch({ type: actionTypes.SET_SUGGESTIONS_LOADING_OFF });
+              setErrorMessage((prev) => ({ ...prev, star: error.message }));
             }
           }
           isSelecting.current = false;
